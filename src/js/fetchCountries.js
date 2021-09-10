4a53275104ba5b8b26a35fad1bcf556f840f7304
Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2021-09-10 18:53:12 +0300

    1

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,3 +2,20 @@
 //     return fetch()
 // }
 // export default fetchCountries;
+// function fetchCountries() {
+//     return fetch('https://restcountries.eu/rest/v2/name/{name}').then(data => {
+//         console.log(
+//         data
+//     )})
+// }
+import countriesCardTps from '../tamplase/countries.hbs'
+
+fetch('https://restcountries.eu/rest/v2/name/eesti').then(response => {
+    return response.json()
+}).then(countries => {
+    console.log(countries)
+    const markUp = countriesCardTps(countries)
+    console.log(markUp)
+}).catch(error => {
+    console.log(error)
+});
