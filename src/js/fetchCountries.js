8899a1db54b368eb440c0d3dc77b30b381f28496
Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2021-10-04 19:57:52 +0300

    1

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 function fetchCountries(searchQuery) {
-    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`
+    const url = `https://restcountries.eu/rest/v3/name/${searchQuery}`
    return fetch(url).then(response => response.json())
 };
 export default fetchCountries;
